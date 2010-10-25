a63defab0c0bb6877ff14b7515694c306e56cbb5
Jean-Daniel Dupas <jd.dupas@ninsight.com>
2010-10-25 17:16:26 +0200

    Import only if needed

--- Sources/Multimedia/WBAudioFunctions.h
+++ Sources/Multimedia/WBAudioFunctions.h
@@ -8,10 +8,10 @@
  *  This file is distributed under the MIT License. See LICENSE.TXT for details.
  */
 
-#import <CoreAudio/CoreAudio.h>
-
 #if !defined(__WB_AUDIO_FUNCTIONS_H)
 #define __WB_AUDIO_FUNCTIONS_H 1
+
+#import <CoreAudio/CoreAudio.h>
 
 #include WBHEADER(WBBase.h)
 
